bb205e0c451e4ddaccb182e2c643c94a1d7fe40b
kookob <kookob@gmail.com>
2016-10-28 12:15:28 +0800

    代码重构，sql日志不再保存到文件，直接输出到console窗口，提升性能。

--- src/restore/sql/RestoreSqlConfig.java
+++ src/restore/sql/RestoreSqlConfig.java
@@ -1,12 +1,9 @@
 package restore.sql;
 
-import java.io.RandomAccessFile;
-
 /**
  * @author ob
  */
 public class RestoreSqlConfig {
     public static boolean running = false;
     public static boolean sqlFormat = false;
-    public static RandomAccessFile sqlFile = null;
 }
